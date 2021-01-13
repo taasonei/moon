6dd1494963f4b1fb67df5db23c34e22e5a10645e
taasonei <taasonei@gmail.com>
2021-01-13 18:42:11 +0300

    added onCkick method, changed app theme to NoActionBar theme

--- app/src/main/java/com/github/taasonei/moon/MainActivity.kt
+++ app/src/main/java/com/github/taasonei/moon/MainActivity.kt
@@ -2,10 +2,24 @@ package com.github.taasonei.moon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val answersList : Array<String> = arrayOf("Да", "Нет")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvAnswer : TextView = findViewById(R.id.tvAnswer)
+        val button:Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            tvAnswer.text = answersList[Random.nextInt(2)]
+        }
     }
+
 }
