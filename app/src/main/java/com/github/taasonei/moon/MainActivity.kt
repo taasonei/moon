efa9b55be991d447fa5f7b9ca083005b186c6c2a
taasonei <taasonei@gmail.com>
2021-01-13 23:06:26 +0300

    started customizing button colors

--- app/src/main/java/com/github/taasonei/moon/MainActivity.kt
+++ app/src/main/java/com/github/taasonei/moon/MainActivity.kt
@@ -8,21 +8,25 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity(), View {
 
     private lateinit var tvAnswer: TextView
+    private lateinit var button: Button
+    private val presenter = Presenter(this, this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tvAnswer = findViewById(R.id.tvAnswer)
-        val presenter = Presenter(this, this)
-        val button: Button = findViewById(R.id.button)
+        button = findViewById(R.id.button)
 
         button.setOnClickListener {
+            button.isEnabled = false
+            Thread.sleep(3000)
             presenter.generateAnswer()
         }
     }
 
     override fun setAnswer(answer: String) {
         tvAnswer.text = answer
+        button.isEnabled = true
     }
 }
--- app/src/main/java/com/github/taasonei/moon/Presenter.kt
+++ app/src/main/java/com/github/taasonei/moon/Presenter.kt
@@ -3,11 +3,13 @@ package com.github.taasonei.moon
 import android.content.Context
 import kotlin.random.Random
 
-class Presenter constructor(private val view: com.github.taasonei.moon.View, private val context: Context){
-
-    private val answers : Array<String> = context.resources.getStringArray(R.array.answers)
+class Presenter(
+    private val view: com.github.taasonei.moon.View,
+    private val context: Context
+) {
 
     fun generateAnswer() {
+        val answers: Array<String> = context.resources.getStringArray(R.array.answers)
         view.setAnswer(answers[Random.nextInt(2)])
     }
 
